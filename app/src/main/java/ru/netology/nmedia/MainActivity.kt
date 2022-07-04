7ed4d5dc8ec467c1cefa932ec20ee8033205bbc3
Sergey Melnichenko <m1s3p@mail.ru>
2022-07-04 14:37:45 +0600

    Update app to MVVM Architecture Cleaning code from unused functions

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,32 +1,26 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.dto.Post
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            author = "Нетология. Университет интернет-профессий будущего",
-            authorAvatar = "",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-        )
+        val viewModel by viewModels<PostViewModel>()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                likeNum.text = CountLikeShare.counterDecimal(post.likeNum)
+                shareNum.text = CountLikeShare.counterDecimal(post.shareNum)
 
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            likeNum.text = CountLikeShare.counterDecimal(post.likeNum)
-            shareNum.text = CountLikeShare.counterDecimal(post.shareNum)
-            like.setOnClickListener {
-                post.liked = !post.liked
                 like.setImageResource(
                     if (post.liked) {
                         R.drawable.ic_baseline_liked
@@ -34,19 +28,11 @@ class MainActivity : AppCompatActivity() {
                         R.drawable.ic_baseline_like
                     }
                 )
-                if (post.liked) {
-                    post.likeNum++
-                } else {
-                    post.likeNum--
-                }
                 likeNum.text = CountLikeShare.counterDecimal(post.likeNum)
-//                likeNum.text = post.likeNum.toString()
-            }
-            share.setOnClickListener {
-                post.shareNum++
-                shareNum.text = CountLikeShare.counterDecimal(post.shareNum)
-//                shareNum.text = post.shareNum.toString()
-//                println("changed")
+
+                like.setOnClickListener { viewModel.like() }
+
+                share.setOnClickListener { viewModel.share() }
             }
         }
     }
