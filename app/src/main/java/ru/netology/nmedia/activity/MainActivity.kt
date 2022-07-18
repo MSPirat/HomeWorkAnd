1de4aaf3da9e39df406fae24a4aedccd813e22a1
Sergey Melnichenko <m1s3p@mail.ru>
2022-07-18 18:34:14 +0600

    Make add new Post & edit Posts in other Activity

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,8 +1,7 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import androidx.activity.result.launch
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
@@ -10,7 +9,6 @@ import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.adapter.PostEventListener
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -22,13 +20,32 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: PostViewModel by viewModels()
 
-        binding.group.visibility = View.GONE
+        val newPostContract = registerForActivityResult(NewPostActivityContract()) { text ->
+            text?.let {
+                viewModel.changeContent(it)
+                viewModel.saveContent()
+            }
+        }
+
+//        val viewModel: PostViewModel by viewModels()
+
+//        binding.group.visibility = View.GONE
+
+        val editPostActivityContract =
+            registerForActivityResult(EditPostActivityContract()) { text ->
+                text?.let {
+                    viewModel.changeContent(it)
+                    viewModel.saveContent()
+                }
+            }
 
         val adapter = PostAdapter(
             object : PostEventListener {
+
                 override fun onEdit(post: Post) {
                     viewModel.editContent(post)
-                    binding.group.visibility = View.VISIBLE
+//                    binding.group.visibility = View.VISIBLE
+                    editPostActivityContract.launch(post.content)
                 }
 
                 override fun onRemove(post: Post) {
@@ -47,46 +64,51 @@ class MainActivity : AppCompatActivity() {
 
         binding.container.adapter = adapter
 
-        viewModel.data.observe(this) { posts ->
+        viewModel.data.observe(this)
+        { posts ->
             val newPost = adapter.itemCount < posts.size
             adapter.submitList(posts) {
                 if (newPost) binding.container.smoothScrollToPosition(0)
             }
         }
 
-        viewModel.edited.observe(this) {
-            with(binding.contentCheck) {
-                text = it.content
-            }
-            binding.content.setText(it.content)
+        binding.addPost.setOnClickListener {
+            newPostContract.launch()
         }
 
-        binding.save.setOnClickListener {
-            with(binding.content) {
-                if (text.isNullOrBlank()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        context.getString(R.string.empty_post_error),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-                viewModel.changeContent(text.toString())
-                viewModel.saveContent()
-                setText("")
-                clearFocus()
-                AndroidUtils.hideKeyboard(this)
-                binding.group.visibility = View.GONE
-            }
-        }
 
-        binding.cancel.setOnClickListener {
-            with(binding.content) {
-                setText("")
-                clearFocus()
-                AndroidUtils.hideKeyboard(this)
-                binding.group.visibility = View.GONE
-            }
-        }
+//        viewModel.edited.observe(this) {
+//            with(binding.contentCheck) {
+//                text = it.content
+//            }
+//            binding.content.setText(it.content)
+//        }
+
+//        binding.save.setOnClickListener {
+//            with(binding.content) {
+////                if (text.isNullOrBlank()) {
+////                    Toast.makeText(
+////                        this@MainActivity,
+////                        context.getString(R.string.empty_post_error),
+////                        Toast.LENGTH_SHORT
+////                    ).show()
+////                    return@setOnClickListener
+////                }
+//                viewModel.changeContent(text.toString())
+//                viewModel.saveContent()
+//                setText("")
+//                clearFocus()
+//                AndroidUtils.hideKeyboard(this)
+//                binding.group.visibility = View.GONE
+//            }
+//        }
+
+//        binding.cancel.setOnClickListener {
+//            with(binding.content) {
+//                setText("")
+//                clearFocus()
+//                AndroidUtils.hideKeyboard(this)
+//                binding.group.visibility = View.GONE
+//        }
     }
 }
