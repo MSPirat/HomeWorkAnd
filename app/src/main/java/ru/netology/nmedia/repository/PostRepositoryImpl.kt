9f9d48e6617e61b428faeaceed4089767963ac7e
[Sergey Melnichenko] <[m1s3@mail.ru]>
2022-12-02 15:19:40 +0600

    Added PostRemoteMediator.kt, changed PostDao.kt

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -1,13 +1,9 @@
 package ru.netology.nmedia.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
+import androidx.paging.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import ru.netology.nmedia.api.ApiService
@@ -29,14 +25,15 @@ class PostRepositoryImpl @Inject constructor(
     private val apiService: ApiService,
 ) : PostRepository {
 
-    override val data = Pager(
+    @OptIn(ExperimentalPagingApi::class)
+    override val data: Flow<PagingData<Post>> = Pager(
         config = PagingConfig(pageSize = 10, enablePlaceholders = false),
-        pagingSourceFactory = {
-            PostPagingSource(
-                apiService
-            )
-        }
+        pagingSourceFactory = { dao.getPagingSource() },
+        remoteMediator = PostRemoteMediator(apiService = apiService, postDao = dao)
     ).flow
+        .map {
+            it.map(PostEntity::toDto)
+        }
 
     override suspend fun getAll() {
         try {
